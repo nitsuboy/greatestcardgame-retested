4c657e459015eed16518c3cc2957f41617ce9095
Nikolas Rian <niko.coder@proton.me>
2025-07-07 14:03:41 -0300

    Fixed TestChalangeEvaluator

--- scripts/ChallengeEvaluator.gd
+++ scripts/ChallengeEvaluator.gd
@@ -6,7 +6,7 @@ static func IsValidTest(test_name: String, desafio_card: CardData) -> bool:
 	return desafio_card.test_data.has(test_name)
 
 static func IsSuccessfulRoll(test_name: String, desafio_card: CardData, roll: int) -> bool:
-	if not desafio_card.test_data.has(test_name):
+	if not desafio_card.test_requirements.has(test_name):
 		return false
-	var valid_numbers: Array[int] = desafio_card.test_data[test_name]
+	var valid_numbers: Array = desafio_card.test_requirements[test_name]
 	return valid_numbers.has(roll)
