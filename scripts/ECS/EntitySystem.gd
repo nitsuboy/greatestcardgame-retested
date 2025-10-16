4124fb9fd335c0a95c5e7ac9bce520a8223fd26f
Samuel-C <samuelcamilo8@gmail.com>
2025-10-15 21:54:10 -0300

    feat(): adicionar Sistema de entidade

--- scripts/ECS/EntitySystem.gd
+++ scripts/ECS/EntitySystem.gd
@@ -1,5 +1,9 @@
 extends System
 class_name EntitySystem
 
-static func HasComp(entity : Entity, comp_type):
-	pass
+static func HasComp(entity : Entity, comp_type : Script) -> bool:
+	for component in entity.components:
+		if component.get_script() == comp_type:
+			return true
+	return false
+	
