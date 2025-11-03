ee77e8a1ff51a002d0610f5aba5190d9cd8277fb
Samuel-C <samuelcamilo8@gmail.com>
2025-11-03 08:49:39 -0300

    misc: move stuff into modules

--- scripts/Input/Systems/input_system.gd
+++ scripts/Input/Systems/input_system.gd
@@ -17,3 +17,24 @@ func _on_gui_input(event: InputEvent) -> void:
 		var left_click_event = MouseRightClickEvent.new(ev_args)
 		left_click_event.start()
 		return
+
+static func handle_gui_input(event: InputEvent, entity: Entity) -> void:
+	var node_comp = EntitySystem.get_comp(entity, NodeComponent)
+	if not node_comp:
+		return
+	
+	var card = node_comp.node
+
+	if event :
+		card.card_is_focused(true)
+	else:
+		card.card_is_focused(false) 
+		return
+
+	# Encaminhar eventos de input para os sistemas adequados
+	if event.is_action_pressed("mouse_left"):
+		DragSystem.on_drag_start(entity)
+		ZoomSystem.on_zoom_start(entity)
+	elif event.is_action_released("mouse_left"):
+		DragSystem.on_drag_end(entity)
+		ZoomSystem.on_zoom_end(entity)
