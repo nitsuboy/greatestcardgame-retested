0a844610147839a3960b097f38f1703f27333e54
Samuel-C <samuelcamilo8@gmail.com>
2025-11-08 11:29:31 -0300

    test: Reescrever o teste para checar mais coisas importantes

--- .github/scripts/ecs_check_structure.py
+++ .github/scripts/ecs_check_structure.py
@@ -5,26 +5,77 @@ green = "\033[32m"
 yellow = "\033[33m"
 reset = "\033[0m"
 
+def verificarModulo(modulo: Path) -> bool:
+    all_good = True
+
+    for pasta in modulo.iterdir():
+        if pasta.is_dir():
+            if pasta.name not in ("components", "events", "systems"):
+                print(f"pasta não padrão: {yellow}{pasta.name}{reset} dentro do modulo {yellow}{modulo.name}{reset} - {red}NOT OK - PASTAS PADRÕES: \"components\", \"events\", \"systems\"{reset}")
+                all_good = False
+            else:
+                print(f"pasta padrão: {yellow}{pasta.name}{reset} dentro do modulo {yellow}{modulo.name}{reset} - {green}OK{reset}")
+                for arquivo in pasta.iterdir():
+                    if arquivo.is_dir():
+                        print(f"pasta: {yellow}{arquivo.name}{reset} dentro da pasta {yellow}{pasta.name}{reset} - {red}NOT OK - APENAS ARQUIVOS PERMITIDOS DENTRO DA PASTA: {pasta.name}{reset}")
+                        all_good = False
+                    else:
+                        if arquivo.name.endswith(".uid"):
+                            continue
+
+                        if pasta.name == "components":
+                            if arquivo.name.endswith("_component.gd"):
+                                print(f"arquivo componente: {yellow}{arquivo.name}{reset} dentro da pasta {yellow}{pasta.name}{reset} - {green}OK{reset}")
+                            else:
+                                print(f"arquivo: {yellow}{arquivo.name}{reset} dentro da pasta {yellow}{pasta.name}{reset} - {red}NOT OK - APENAS ARQUIVOS TERMINADOS EM \"_component.gd\" PERMITIDOS NA PASTA: {pasta.name}{reset}")
+                                all_good = False
+
+                        elif pasta.name == "events":
+                            if arquivo.name.endswith("_event.gd"):
+                                print(f"arquivo evento: {yellow}{arquivo.name}{reset} dentro da pasta {yellow}{pasta.name}{reset} - {green}OK{reset}")
+                            elif arquivo.name.endswith("_event_args.gd"):
+                                print(f"arquivo argumento de evento: {yellow}{arquivo.name}{reset} dentro da pasta {yellow}{pasta.name}{reset} - {green}OK{reset}")
+                            else:
+                                print(f"arquivo: {yellow}{arquivo.name}{reset} dentro da pasta {yellow}{pasta.name}{reset} - {red}NOT OK - APENAS ARQUIVOS TERMINADOS EM \"_event.gd\" ou \"_event_args.gd\" PERMITIDOS NA PASTA: {pasta.name}{reset}")
+                                all_good = False
+        
+                        elif pasta.name == "systems":
+                            if arquivo.name.endswith("_system.gd"):
+                                print(f"arquivo sistema: {yellow}{arquivo.name}{reset} dentro da pasta {yellow}{pasta.name}{reset} - {green}OK{reset}")
+                            else:
+                                print(f"arquivo: {yellow}{arquivo.name}{reset} dentro da pasta {yellow}{pasta.name}{reset} - {red}NOT OK - APENAS ARQUIVOS TERMINADOS EM \"_system.gd\" PERMITIDOS NA PASTA: {pasta.name}{reset}")
+                                all_good = False
+        else:
+            # geralmente modulos não podem ter nenhum arquivo extra além das pastas components, events e systems. Com exceção do modulo ECS
+
+            if modulo.name == "ECS" and pasta.name in (
+                "component.gd", "component.gd.uid",
+                "entity.gd", "entity.gd.uid",
+                "event_args.gd", "event_args.gd.uid",
+                "event.gd", "event.gd.uid",
+                "system.gd", "system.gd.uid"):
+                continue
+
+            print(f"arquivo: {yellow}{pasta.name}{reset} dentro do modulo {yellow}{modulo.name}{reset} - {red}NOT OK - APENAS PASTAS PERMITIDAS DENTRO DO MODULOS{reset}")
+            all_good = False
+    
+    return all_good
+
 def main():
     root = Path("scripts")
     all_good = True
 
-    for arquivo in root.rglob("*"):
-        good = True
+    for pasta in root.iterdir():
+        if pasta.is_dir():
+            modulo = False
 
-        if arquivo.name.endswith(".uid"):
-            continue
+            # Se qualquer pasta tiver uma pasta components, events ou systems, é considerada um módulo
+            for pastaInterna in pasta.iterdir():
+                if pastaInterna.name in ("components", "events", "systems"):
+                    modulo = True
 
-        if arquivo.name.endswith("_event.gd") and arquivo.parent.name != "events":
-            good, all_good = False, False
-        if arquivo.name.endswith("_event_args.gd") and arquivo.parent.name != "events":
-            good, all_good = False, False
-        if arquivo.name.endswith("_component.gd") and arquivo.parent.name != "components":
-            good, all_good = False, False
-        if arquivo.name.endswith("_system.gd") and arquivo.parent.name != "systems":
-            good, all_good = False, False
-
-        print(f"Arquivo: {yellow}{arquivo.name}{reset} - Pasta acima: {yellow}{arquivo.parent.name}{reset} - {green + "OK" if good else red + "NOT OK"}{reset}")
+            if modulo:
+                all_good = False if not verificarModulo(pasta) else all_good
 
     return 0 if all_good else 1
 
