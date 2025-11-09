c3834614aafbae0199aae9929d94a95c6854b3b9
Samuel-C <samuelcamilo8@gmail.com>
2025-11-09 11:18:53 -0300

    misc: added back, but now fixed

---
+++ scripts/input/events/card_input_event.gd
@@ -0,0 +1,28 @@
+class_name CardInputEvent
+extends Event
+
+
+func _init(event_args: CardInputEventArgs = null) -> void:
+	args = event_args
+	targets = [event_args.entity]
+
+
+func treat(_entity: Entity) -> void:
+	var comp: Component
+
+	comp = EntitySystem.get_comp(_entity, ZoomableComponent)
+
+	comp = EntitySystem.get_comp(_entity, HoverbleComponent)
+	if comp:
+		HoverSystem.handle_gui_input(_entity, comp, args)
+
+	if not args.input_event:
+		return
+
+	comp = EntitySystem.get_comp(_entity, ZoomableComponent)
+	if comp:
+		ZoomSystem.handle_gui_input(_entity, comp, args)
+
+	comp = EntitySystem.get_comp(_entity, DraggableComponent)
+	if comp:
+		DragSystem.handle_gui_input(_entity, comp, args)
---
+++ scripts/input/systems/input_system.gd
@@ -0,0 +1,3 @@
+class_name InputSystem extends System
+
+# FUTURE: MAKE GUI_INPUT COME FROM HERE
