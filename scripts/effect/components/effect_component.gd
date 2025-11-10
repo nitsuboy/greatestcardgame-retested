67314dbaff63261e41b15b7ec97d18729a356369
Nicolas Carneiro <65839195+nitsuboy@users.noreply.github.com>
2025-11-10 16:51:09 -0300

    fix: formatation

--- scripts/effect/components/effect_component.gd
+++ scripts/effect/components/effect_component.gd
@@ -1,4 +1,4 @@
 class_name EffectComponent
 extends Component
 
-@export var effects : Array[Effect]
+@export var effects: Array[Effect]
--- scripts/effect/systems/effect_system.gd
+++ scripts/effect/systems/effect_system.gd
@@ -1,6 +1,7 @@
 class_name EffectSystem
 extends System
 
+
 static func apply(comp: EffectComponent, node: Node, from: Variant, to: Variant) -> void:
 	for e in comp.effects:
-		e.apply_effect(node,from,to)
+		e.apply_effect(node, from, to)
