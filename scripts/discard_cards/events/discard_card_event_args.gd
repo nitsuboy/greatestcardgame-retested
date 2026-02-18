9be944835ec04abeef1f45eed20806a0234928d7
Samuel-C <samuelcamilo8@gmail.com>
2026-02-18 10:24:55 -0300

    misc: add constructor method to discard card event args

--- scripts/discard_cards/events/discard_card_event_args.gd
+++ scripts/discard_cards/events/discard_card_event_args.gd
@@ -2,4 +2,8 @@ class_name DiscardCardEventArgs
 extends EventArgs
 
 var card_entity: Entity
-var dropzone: DropZone
+var discardzone: DropZone
+
+func _init(_card_entity: Entity, _discardzone: DropZone):
+	card_entity = _card_entity
+	discardzone = _discardzone
