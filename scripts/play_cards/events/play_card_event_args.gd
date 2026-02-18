f288207459b8f16b770d5eef97230b5bdb09dc1e
Samuel-C <samuelcamilo8@gmail.com>
2026-02-18 10:29:18 -0300

    fix: start the play card event

--- scripts/play_cards/events/play_card_event_args.gd
+++ scripts/play_cards/events/play_card_event_args.gd
@@ -2,4 +2,8 @@ class_name PlayCardEventArgs
 extends EventArgs
 
 var card_entity: Entity
-var dropzone: DropZone
+var playzone: DropZone
+
+func _init(_card_entity: Entity, _playzone: DropZone):
+	card_entity = _card_entity
+	playzone = _playzone
