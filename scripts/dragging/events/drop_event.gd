169c74101a5702b4e470ab1d041cd29db9e73375
Samuel-C <samuelcamilo8@gmail.com>
2026-02-18 10:34:54 -0300

    misc: call try discard method

--- scripts/dragging/events/drop_event.gd
+++ scripts/dragging/events/drop_event.gd
@@ -13,3 +13,8 @@ func treat(_entity: Entity) -> void:
 	comp = EntitySystem.get_comp(_entity, PlayableComponent)
 	if comp:
 		PlayCardSystem.try_play_card(_entity, comp, args)
+	
+	comp = EntitySystem.get_comp(_entity, DiscardableComponent)
+	if comp:
+		DiscardCardSystem.try_discard_card(_entity, comp, args)	
+	
