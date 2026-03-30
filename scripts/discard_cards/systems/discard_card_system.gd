54cb272e23d9675eb42428bb5da5e9abe02dddce
Samuel-C <samuelcamilo8@gmail.com>
2026-03-30 16:53:05 -0300

    misc: dever de casa para outra pessoa

--- scripts/discard_cards/systems/discard_card_system.gd
+++ scripts/discard_cards/systems/discard_card_system.gd
@@ -1,6 +1,7 @@
 class_name DiscardCardSystem
 extends System
 
+# esse sistema todo provavelmente precisa de uma refatoração
 
 static func try_discard_card(
 	entity: Entity, _comp: DiscardableComponent, event_args: DropEventArgs
@@ -8,10 +9,10 @@ static func try_discard_card(
 	var dropzone = event_args.drop_zone
 
 	if EntitySystem.has_comp(dropzone.entity, DiscardZoneComponent):
-		NetworkManager.request_action(1, 0, entity.id, dropzone.entity.id)
+		NetworkManager.request_action(1, 0, entity.id, dropzone.entity.id) # TODO: colocar os enums aqui
 
 
-static func discard_card(entity: Entity, _comp: Component) -> void:
+static func discard_card(entity: Entity, _comp: NodeComponent) -> void: # TODO: substituir com `comp: DiscardableComponent`
 	var dealer = NetworkManager.game._dealer
 	dealer.discard_card(_comp.node)
 
@@ -22,7 +23,7 @@ static func discard_card(entity: Entity, _comp: Component) -> void:
 	print("discard feito")
 
 
-static func discard_card_request(entity: Entity, _comp: Component, _event_args: EventArgs) -> void:
+static func discard_card_request(entity: Entity) -> void: # TODO: adicionar `comp: DiscardableComponent`
 	if NetworkManager.multiplayer.is_server():
 		NetworkManager.request_action(
 			NetworkManager.ActionWhere.GAME, GameManager.Actions.DISCARD_CARD, entity.id
