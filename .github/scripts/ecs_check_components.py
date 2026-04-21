66627d2968a81cbb17f983c834e1c9e32cf135c4
Samuka <47865393+Samuka-C@users.noreply.github.com>
2026-04-21 17:12:43 -0300

    Atualização dos teste de ECS para aceitar classes internas (#64)
    * misc: classe interna em componentes
    
    * misc: checar herança de qualquer arquivo godot in scripts
    
    * style: fix lines
    
    * misc: funcoes para checar funcões internas
    
    * misc: classe interna em sistemas
    
    * misc: usar metodos atualizados
    
    * style: whitespace aleatorio
    
    * style: tirar espaço branco
    
    * style: tirar espaço branco

--- .github/scripts/ecs_check_components.py
+++ .github/scripts/ecs_check_components.py
@@ -2,6 +2,10 @@ from pathlib import Path
 import sys
 import re
 
+from godot_scripts_check_inheritance import checarClasseHerdaDeComponent
+from ecs_check_internal_class import checarNomeClasseInterna
+from ecs_check_internal_class import checarHerancaClasseInterna
+
 red = "\033[31m"
 green = "\033[32m"
 yellow = "\033[33m"
@@ -26,14 +30,15 @@ def checarComponente(arquivo: Path) -> int:
     linhas = arquivo.read_text().split("\n")
 
     nomeClasse = None
-    
+    nomeClasseInterna = None
+
     nomeArquivo = arquivo.name.removesuffix("_component.gd")
     indentificadores = nomeArquivo.split("_")
     nomeClassePadrao = ""
     for indentificador in indentificadores:
         nomeClassePadrao += indentificador.capitalize()
     nomeClassePadrao += "Component"
-    
+
     for index, linha in enumerate(linhas):
         linhaNumero = index + 1
 
@@ -46,62 +51,46 @@ def checarComponente(arquivo: Path) -> int:
                 print(f"linha {linhaNumero}: nome classe: {yellow}{nomeClasse}{reset} diferente do padrão: {yellow}{nomeClassePadrao}{reset} - {red}NOT OK{reset}")
                 print(f"{red}o nome da classe deve seguir o padrão do nome do arquivo!{reset}")
                 numeroErros += 1
-        
+
+        # matches the line: class [Something]
+        matchClass = re.search(r"class\s+(\w+)", linha)
+        if matchClass:
+            nomeClasseInterna = matchClass.group(1)
+            numeroErros += checarNomeClasseInterna(nomeClasseInterna, linhaNumero)
+
         # matches the line: extends [Something]
         matchClassParent = re.search(r"extends\s+(\w+)", linha)
         if matchClassParent:
             nomeClassePai = matchClassParent.group(1)
 
-            if not checarClasseHerdaDeComponent(nomeClassePai):
+            if not nomeClasseInterna and not checarClasseHerdaDeComponent(nomeClassePai):
                 print(f"linha {linhaNumero}: classe pai: {yellow}{nomeClassePai}{reset} não herda de {yellow}Component{reset} - {red}NOT OK{reset}")
                 print(f"{red}todas os componentes devem herdar de Component (mesmo que indiretamente)!{reset}")
                 numeroErros += 1
+            elif nomeClasseInterna:
+                numeroErros += checarHerancaClasseInterna(nomeClasseInterna, nomeClassePai, linhaNumero)
 
         # matches the line: func [Something]
         matchFunc = re.search(r"func\s+(\w+)", linha)
         if matchFunc:
             nomeFuncao = matchFunc.group(1)
-            print(f"linha {linhaNumero}: função: {yellow}{nomeFuncao}{reset} dentro de {yellow}{nomeClasse}{reset} - {red}NOT OK{reset}")
-            print(f"{red}componentes não podem ter funções!{reset}")
+
+            if not nomeClasseInterna:
+                print(f"linha {linhaNumero}: função: {yellow}{nomeFuncao}{reset} dentro de {yellow}{nomeClasse}{reset} - {red}NOT OK{reset}")
+                print(f"{red}componentes não podem ter funções!{reset}")
+            else:
+                print(f"linha {linhaNumero}: função: {yellow}{nomeFuncao}{reset} dentro de {yellow}{nomeClasseInterna}{reset} - {red}NOT OK{reset}")
+                print(f"{red}classes internas não podem ter funções!{reset}")
+
             numeroErros += 1
 
-        # matches the line: class [Something]
-        matchClass = re.search(r"class\s+(\w+)", linha)
-        if matchClass:
-            nomeClasseSecundaria = matchClass.group(1)
-
-            print(f"linha {linhaNumero}: classe secundaria {yellow}{nomeClasseSecundaria}{reset} dentro de {yellow}{arquivo.name}{reset} - {red}NOT OK{reset}")
-            print(f"{red}só pode haver uma classe em arquivos de componentes!{reset}")
-            numeroErros += 1
+    if not nomeClasse:
+        print(f"não encontrado linha com {yellow}class_name {nomeClassePadrao}{reset} - {red}NOT OK{reset}")
+        print(f"{red}arquivos de componente devem declarar a classe daquele componente!{reset}")
+        numeroErros += 1
 
     print(f"arquivo componente: {yellow}{arquivo.name}{reset} - {green+"OK" if numeroErros == 0 else red+"NOT OK - " + str(numeroErros) + " erros"}{reset}")
     return numeroErros
-
-def checarClasseHerdaDeComponent(nomeClassePai: str) -> bool:
-    root = Path("scripts")
-
-    if nomeClassePai == "Component":
-        return True
-    
-    for arquivo in root.rglob("*"):
-        if arquivo.name.endswith("_component.gd"):
-            linhas = arquivo.read_text().split("\n")
-
-            arquivoNomeClasse = None
-
-            for linha in linhas:
-                # matches the line: class_name [Something]
-                matchClassName = re.search(r"class_name\s+(\w+)", linha)
-                if matchClassName:
-                    arquivoNomeClasse = matchClassName.group(1)
-                
-                # matches the line: extends [Something]
-                matchClassParent = re.search(r"extends\s+(\w+)", linha)
-                if matchClassParent:
-                    arquivoNomeClassePai = matchClassParent.group(1)
-
-                    if arquivoNomeClasse == nomeClassePai:
-                        return checarClasseHerdaDeComponent(arquivoNomeClassePai)
 
 if __name__ == "__main__":
     main()
