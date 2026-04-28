79ec22071f61f7bf5117512880d9d54f38a86805
Samuel-C <samuelcamilo8@gmail.com>
2026-04-28 09:45:15 -0300

    misc: update pre draw card event

--- scripts/game/draw_cards/events/pre_draw_card_event.gd
+++ scripts/game/draw_cards/events/pre_draw_card_event.gd
@@ -2,14 +2,8 @@ class_name PreDrawCardEvent
 extends Event
 
 
-func _init(event_args: PreDrawCardEventArgs = null) -> void:
-	args = event_args
-	targets = []
+var player_id: int
 
 
-func treat(_entity: Entity) -> void:
-	var comp: Component
-
-	comp = EntitySystem.get_comp(_entity, TriggerOnPreDrawComponent)
-	if comp:
-		TriggerSystem.try_trigger(_entity, comp, args)
+func _init(_player_id: int) -> void:
+	player_id = _player_id
--- scripts/game/draw_cards/events/pre_draw_card_event_args.gd
+++
@@ -1,8 +0,0 @@
-class_name PreDrawCardEventArgs
-extends EventArgs
-
-var player_id: int
-
-
-func _init(_player_id: int) -> void:
-	player_id = _player_id
