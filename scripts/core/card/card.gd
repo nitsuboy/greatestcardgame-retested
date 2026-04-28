51a37adc0b7cf32b55ea4dd2add71b1da23a9f2b
Samuel-C <samuelcamilo8@gmail.com>
2026-04-28 10:02:43 -0300

    misc: update event call in card

--- scripts/core/card/card.gd
+++ scripts/core/card/card.gd
@@ -83,9 +83,8 @@ func _on_gui_input(event: InputEvent) -> void:
 		return
 
 	# TODO: move this to input system. please don't let it be here
-	var e_args = CardInputEventArgs.new(event, entity)
-	var e = CardInputEvent.new(e_args)
-	e.start()
+	var ev = CardInputEvent.new(event, entity)
+	EventSystem.IniciarEventoLocal(entity, ev)
 
 
 func _on_mouse_exited() -> void:
@@ -93,9 +92,8 @@ func _on_mouse_exited() -> void:
 		return
 
 	# TODO: move this to input system. please don't let it be here
-	var e_args = CardInputEventArgs.new(null, entity)
-	var e = CardInputEvent.new(e_args)
-	e.start()
+	var ev = CardInputEvent.new(null, entity)
+	EventSystem.IniciarEventoLocal(entity, ev)
 
 
 # procedural animation
