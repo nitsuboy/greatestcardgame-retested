2f84d5d48382f67b92c23642b0fae3051d469b97
Samuel-C <samuelcamilo8@gmail.com>
2026-04-28 10:29:14 -0300

    misc: update turn system with trigger stuff

--- scripts/game/trigger/systems/trigger_system.gd
+++ scripts/game/trigger/systems/trigger_system.gd
@@ -17,49 +17,13 @@ static func on_trigger_log(_entity: Entity, _comp: LogOnTriggerComponent, _args:
 		return
 	print("    [LOG] %s" % _comp.msg)
 
+
 static func try_trigger(entity: Entity, comp: TriggerOnComponent, _args: Event) -> void:
 	print("=== TriggerSystem: try_trigger ===")
 	print("  Entity: %d | Comp key_out: %s" % [entity.id, comp.key_out])
 	
 	var ev = TriggerEvent.new(comp.key_out)
 	EventSystem.IniciarEventoLocal(entity, ev)
-
-	var on_trigger_comps = EntitySystem.get_comps_related(entity, OnTriggerComponent)
-
-	for on_trigger_comp in on_trigger_comps:
-		if on_trigger_comp.keys_in.has(comp.key_out):
-			var comp_name = on_trigger_comp.get_script().get_global_name().split("/")[-1]
-			print("  [FOUND] Trigger: %s | Key: %s" % [comp_name, comp.key_out])
-
-			match on_trigger_comp.get_script():
-				DrawOnTriggerComponent:
-					var draw_comp = on_trigger_comp as DrawOnTriggerComponent
-					var game = Net.game
-					var action = TriggerAction.new(
-						game.search_player(draw_comp.player),
-						GameManager.Actions.DRAW_CARD,
-						[draw_comp.number_of_cards, draw_comp.player]
-					)
-					Net.enqueue_trigger_action(action)
-					print(
-						(
-							"    [ENQUEUED] DRAW_CARD | Cards: %d | Target offset: %d"
-							% [draw_comp.number_of_cards, draw_comp.player]
-						)
-					)
-
-				SkipTurnOnTriggerComponent:
-					var skip_comp = on_trigger_comp as SkipTurnOnTriggerComponent
-					var action = TriggerAction.new(
-						Net.multiplayer.get_unique_id(),
-						GameManager.Actions.SKIP_TURN,
-						[skip_comp.num_of_turns]
-					)
-					Net.enqueue_trigger_action(action)
-					print("    [ENQUEUED] SKIP_TURN | Turns: %d" % skip_comp.num_of_turns)
-
-				_:
-					push_warning("not in the action list: %s" % str(on_trigger_comp.get_script()))
 
 	print("  Queue size: %d" % Net.get_trigger_queue_size())
 	if not Net.is_trigger_queue_empty():
