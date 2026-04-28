0a63bd7d01244586a62fdc02164a12625d7a1bda
Samuel-C <samuelcamilo8@gmail.com>
2026-04-28 10:03:00 -0300

    misc: make GameManager into abstract

--- scripts/core/game_manager.gd
+++ scripts/core/game_manager.gd
@@ -1,4 +1,4 @@
-class_name GameManager
+@abstract class_name GameManager
 extends Node
 
 enum Actions {
@@ -12,3 +12,5 @@ enum Actions {
 	SKIP_TURN,
 	START_TURN
 }
+
+@abstract func get_dealer() -> Dealer
