0e0185b7f564b6ca420c95010a9879fae2e4c22b
Samuel-C <samuelcamilo8@gmail.com>
2026-04-28 11:13:31 -0300

    misc: update event check

--- .github/scripts/ecs_check_events.py
+++ .github/scripts/ecs_check_events.py
@@ -2,7 +2,9 @@ from pathlib import Path
 import sys
 import re
 
-from ecs_check_event_args import checarClasseHerdaDeEventArgs
+from godot_scripts_check_inheritance import checarClasseHerdaDeEvent
+from ecs_check_internal_class import checarNomeClasseInterna
+from ecs_check_internal_class import checarHerancaClasseInterna
 
 red = "\033[31m"
 green = "\033[32m"
@@ -27,17 +29,15 @@ def checarEventos(arquivo: Path) -> int:
 
     linhas = arquivo.read_text().split("\n")
 
-    nomeClasseEvento = None
-    nomeClasseArgumentoEvento = None
-
-    dentroFuncao = False
+    nomeClasse = None
+    nomeClasseInterna = None
     
     nomeArquivo = arquivo.name.removesuffix("_event.gd")
     indentificadores = nomeArquivo.split("_")
-    nomeClasseEventoPadrao = ""
+    nomeClassePadrao = ""
     for indentificador in indentificadores:
-        nomeClasseEventoPadrao += indentificador.capitalize()
-    nomeClasseEventoPadrao += "Event"
+        nomeClassePadrao += indentificador.capitalize()
+    nomeClassePadrao += "Event"
 
     for index, linha in enumerate(linhas):
         linhaNumero = index + 1
@@ -45,101 +45,53 @@ def checarEventos(arquivo: Path) -> int:
         # matches the line: class_name [Something]
         matchClassName = re.search(r"class_name\s+(\w+)", linha)
         if matchClassName:
-            nomeClasseEvento = matchClassName.group(1)
+            nomeClasse = matchClassName.group(1)
 
-            if nomeClasseEvento != nomeClasseEventoPadrao:
-                print(f"linha {linhaNumero}: nome classe de evento: {yellow}{nomeClasseEvento}{reset} diferente do padrão: {yellow}{nomeClasseEventoPadrao}{reset} - {red}NOT OK{reset}")
+            if nomeClasse != nomeClassePadrao:
+                print(f"linha {linhaNumero}: nome classe: {yellow}{nomeClasse}{reset} diferente do padrão: {yellow}{nomeClassePadrao}{reset} - {red}NOT OK{reset}")
                 print(f"{red}o nome da classe deve seguir o padrão do nome do arquivo!{reset}")
                 numeroErros += 1
 
         # matches the line: class [Something]
         matchClass = re.search(r"class\s+(\w+)", linha)
         if matchClass:
-            nomeClasseArgumentoEvento = matchClass.group(1)
-
-            dentroFuncao = False
-
-            matchRegularEventArgName = re.search(r"^([A-Z][a-z0-9]*)+EventArgs$", nomeClasseArgumentoEvento)
-
-            if not matchRegularEventArgName:
-                print(f"linha {linhaNumero}: nome classe de argumento de evento: {yellow}{nomeClasseArgumentoEvento}{reset} fora do padrão {yellow}CamelCaseEventArgs{reset} - {red}NOT OK{reset}")
-                print(f"{red}o nome da classe de argumento de evento deve seguir o padrão \"CamelCaseEventArgs\"{reset}")
-                numeroErros += 1
+            nomeClasseInterna = matchClass.group(1)
+            numeroErros += checarNomeClasseInterna(nomeClasseInterna, linhaNumero)
 
         # matches the line: extends [Something]
         matchClassParent = re.search(r"extends\s+(\w+)", linha)
         if matchClassParent:
             nomeClassePai = matchClassParent.group(1)
 
-            if not nomeClasseArgumentoEvento:
-                if not checarClasseHerdaDeEvent(nomeClassePai):
-                    print(f"linha {linhaNumero}: classe pai: {yellow}{nomeClassePai}{reset} de {yellow}{nomeClasseEvento}{reset} não herda de {yellow}Event{reset} - {red}NOT OK{reset}")
-                    print(f"{red}todas os eventos devem herdar de Event (mesmo que indiretamente)!{reset}")
-                    numeroErros += 1
-            else:
-                if not checarClasseHerdaDeEventArgs(nomeClassePai):
-                    print(f"linha {linhaNumero}: classe pai: {yellow}{nomeClassePai}{reset} de {yellow}{nomeClasseArgumentoEvento}{reset} não herda de {yellow}EventArgs{reset} - {red}NOT OK{reset}")
-                    print(f"{red}todas os argumentos de evento devem herdar de EventArgs (mesmo que indiretamente)!{reset}")
-                    numeroErros += 1
+            if not nomeClasseInterna and not checarClasseHerdaDeEvent(nomeClassePai):
+                print(f"linha {linhaNumero}: classe pai: {yellow}{nomeClassePai}{reset} não herda de {yellow}Event{reset} - {red}NOT OK{reset}")
+                print(f"{red}todos os eventos devem herdar de Event (mesmo que indiretamente)!{reset}")
+                numeroErros += 1
+            elif nomeClasseInterna:
+                numeroErros += checarHerancaClasseInterna(nomeClasseInterna, nomeClassePai, linhaNumero)
 
         # matches the line: func [Something]
         matchFunc = re.search(r"func\s+(\w+)", linha)
         if matchFunc:
             nomeFuncao = matchFunc.group(1)
 
-            dentroFuncao = True
-
-            if not nomeClasseArgumentoEvento:
-                if nomeFuncao not in ("_init", "treat"):
-                    print(f"linha {linhaNumero}: função: {yellow}{nomeFuncao}{reset} dentro de {yellow}{nomeClasseEvento}{reset} - {red}NOT OK{reset}")
-                    print(f"{red}eventos só podem ter a função _init e treat{reset}")
+            if nomeFuncao != "_init":
+                if not nomeClasseInterna:
+                    print(f"linha {linhaNumero}: função: {yellow}{nomeFuncao}{reset} dentro de {yellow}{nomeClasse}{reset} - {red}NOT OK{reset}")
+                    print(f"{red}eventos só podem ter a função _init!{reset}")
                     numeroErros += 1
-            else:
-                if nomeFuncao != "_init":
-                    print(f"linha {linhaNumero}: função: {yellow}{nomeFuncao}{reset} dentro de {yellow}{nomeClasseArgumentoEvento}{reset} - {red}NOT OK{reset}")
-                    print(f"{red}argumentos de evento só podem ter a função _init{reset}")
+                else:
+                    print(f"linha {linhaNumero}: função: {yellow}{nomeFuncao}{reset} dentro de {yellow}{nomeClasseInterna}{reset} - {red}NOT OK{reset}")
+                    print(f"{red}classes internas só podem ter a função _init!{reset}")
                     numeroErros += 1
 
-        # matches the line: var [Something]
-        matchVar = re.search(r"(\t+)?var\s+(\w+)", linha)
-        if matchVar:
-            tabs = matchVar.group(1)
-            nomeVar = matchVar.group(2)
-
-            if not nomeClasseArgumentoEvento:
-                if not tabs or not dentroFuncao:
-                    print(f"linha {linhaNumero}: variável {yellow}{nomeVar}{reset} dentro de {yellow}{nomeClasseEvento}{reset} - {red}NOT OK{reset}")
-                    print(f"{red}eventos não podem ter variáveis!{reset}")
-                    numeroErros += 1
+    if not nomeClasse:
+        print(f"não encontrado linha com {yellow}class_name {nomeClassePadrao}{reset} - {red}NOT OK{reset}")
+        print(f"{red}arquivos de evento devem declarar a classe daquele evento!{reset}")
+        numeroErros += 1
 
     print(f"arquivo evento: {yellow}{arquivo.name}{reset} - {green+"OK" if numeroErros == 0 else red+"NOT OK - " + str(numeroErros) + " erros"}{reset}")
     return numeroErros
-
-def checarClasseHerdaDeEvent(nomeClassePai: str) -> bool:
-    root = Path("scripts")
-
-    if nomeClassePai == "Event":
-        return True
-    
-    for arquivo in root.rglob("*"):
-        if arquivo.name.endswith("_event.gd"):
-            linhas = arquivo.read_text().split("\n")
-
-            arquivoNomeClasse = None
-
-            for linha in linhas:
-                # matches the line: class_name [Something]
-                matchClassName = re.search(r"class_name\s+(\w+)", linha)
-                if matchClassName:
-                    arquivoNomeClasse = matchClassName.group(1)
-                
-                # matches the line: extends [Something]
-                matchClassParent = re.search(r"extends\s+(\w+)", linha)
-                if matchClassParent:
-                    arquivoNomeClassePai = matchClassParent.group(1)
-
-                    if arquivoNomeClasse == nomeClassePai:
-                        return checarClasseHerdaDeEventArgs(arquivoNomeClassePai)
 
 if __name__ == "__main__":
     main()
