9be8898f2f031b4bad22c6628e8e4ee0f189edca
Samuel-C <samuelcamilo8@gmail.com>
2026-05-04 15:18:30 -0300

    misc: update Entity

--- scripts/ECS/entity.gd
+++ scripts/ECS/entity.gd
@@ -1,21 +1,7 @@
 class_name Entity
 extends RefCounted
+## entites are the base of the ECS system. by themselves, they don't have much,
+## however their data and behavior is stored in components
 
-var id: int
-var components: Array[Component] = []
-
-
-func _init(_id: int = -1) -> void:
-	if Globals.debug or (_id == -1 and Net.multiplayer.is_server()):
-		EntityRegistry.calculate_next_id()
-		id = EntityRegistry.next_id
-		EntityRegistry.next_id += 1
-		EntityRegistry._all_entities[id] = self
-	elif _id > -1:
-		id = _id
-		EntityRegistry._all_entities[id] = self
-
-
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_PREDELETE:
-		print("sayonara")
+var uid: int
+var deleted: bool = false
