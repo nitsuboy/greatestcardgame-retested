a45402c9ceaafc818d2bd0f110dbff6cc0a724eb
unknown <caneiroassado@gmail.com>
2026-05-06 12:22:58 -0300

    chore: format

--- scripts/ECS/systems/query_system.gd
+++ scripts/ECS/systems/query_system.gd
@@ -51,7 +51,9 @@ static func all_entities_with_comp2(comp_type1: Script, comp_type2: Script) -> A
 
 
 ## Retorna todas as entidades que tem tres determinados componentes
-static func all_entities_with_comp3(comp_type1: Script, comp_type2: Script, comp_type3: Script) -> Array[Entity]:
+static func all_entities_with_comp3(
+	comp_type1: Script, comp_type2: Script, comp_type3: Script
+) -> Array[Entity]:
 	var aux: Array[Entity] = []
 	var comp1_dict = ComponentRegistry.get_component_registry().get(comp_type1)
 	var comp2_dict = ComponentRegistry.get_component_registry().get(comp_type2)
@@ -71,7 +73,9 @@ static func all_entities_with_comp3(comp_type1: Script, comp_type2: Script, comp
 
 
 ## Retorna todas as entidades que tem quatro determinados componentes
-static func all_entities_with_comp4(comp_type1: Script, comp_type2: Script, comp_type3: Script, comp_type4: Script) -> Array[Entity]:
+static func all_entities_with_comp4(
+	comp_type1: Script, comp_type2: Script, comp_type3: Script, comp_type4: Script
+) -> Array[Entity]:
 	var aux: Array[Entity] = []
 	var comp1_dict = ComponentRegistry.get_component_registry().get(comp_type1)
 	var comp2_dict = ComponentRegistry.get_component_registry().get(comp_type2)
@@ -79,7 +83,11 @@ static func all_entities_with_comp4(comp_type1: Script, comp_type2: Script, comp
 	var comp4_dict = ComponentRegistry.get_component_registry().get(comp_type4)
 
 	for entity_uid in comp1_dict.keys():
-		if not comp2_dict.has(entity_uid) or not comp3_dict.has(entity_uid) or not comp4_dict.has(entity_uid):
+		if (
+			not comp2_dict.has(entity_uid)
+			or not comp3_dict.has(entity_uid)
+			or not comp4_dict.has(entity_uid)
+		):
 			continue
 
 		var entity = EntityRegistry.get_entity(entity_uid)
