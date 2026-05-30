30ed7eb6e6d3a3bc05ceb445e82542cbb14c0aea
unknown <caneiroassado@gmail.com>
2026-05-30 11:07:47 -0300

    hotfix: cards not ignoring others
    cards stoped ignorinothers when passing trhough

--- addons/truco/core/input/hover/hover_system.gd
+++ addons/truco/core/input/hover/hover_system.gd
@@ -22,6 +22,11 @@ func _on_card_mouse_entered(entity_id: int) -> void:
 
 
 func _on_card_mouse_exited(entity_id: int) -> void:
+	if (
+		not world.has_component(entity_id, HoverableComponent)
+		or (world.get_storage(DragState) != null and world.get_storage(DragState).size() > 0)
+	):
+		return
 	_on_hover_end(entity_id)
 
 
